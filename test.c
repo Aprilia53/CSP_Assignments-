8ff4f5c77461a0277c988026e587274f293d0e9b
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-09-11 19:14:46 +0000

    trying to figure out why its printing a number

--- test.c
+++ test.c
@@ -3,6 +3,6 @@ int main() {
    char name[20];
    printf("what is your name?: \n");
    scanf("%d", name);
-   printf("your name is:%d", name);
+   printf("your name is: %d", name);
     return 0;
 }
