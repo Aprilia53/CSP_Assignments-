f38f1297fe4eab845cf632f00cc7506e560208b3
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-09-17 02:41:10 +0000

    made a variable

--- expressions/budget.py
+++ expressions/budget.py
@@ -3,7 +3,7 @@ income = input()
 print("your monthly income is", income ) 
 
 print("How much is your rent each month? ")
-rent= input() 
+rent = input() 
 
 print("How much is your utilities each month?")
 utilities = input() 
@@ -14,20 +14,30 @@ groceries = input()
 print("What is your monthly transportation price?") 
 transportation = input() 
 
-expenses=float(rent)+float(utilities)+float(groceries)+float(transportation) 
+expenses = float(rent) + float(utilities) + float(groceries) + float(transportation)
 print("your monthly expences are ", expenses)
 
-savings= income*(0.3) 
-print("Your monthly savings are", savings)
+savings = income*(0.3) 
+print("your savings are", savings  (" percent of your monthly income") )
 
-spendingMoney= income*(0.1) 
-print("Your monthly spending money is ", spendingMoney )
+spendingMoney = income*(0.1) 
+print("your spending money is", spendingMoney  (" percent of your monthly income") )
 
-Rent= income*(0.2) 
-print("your monthly rent is", Rent) 
+Rent = income*(0.2) 
+print("your rent are", rent  (" percent of your monthly income") )
+
+Utilities = income*(0.2)
+print("your utilities are", Utilities  (" percent of your monthly income") )
+
+Groceries = income*(0.1)
+print("your groceries are", Groceries  (" percent of your monthly income") )
+
+Transportation = income*(0.1)
+print("your transportation cost is", savings  (" percent of your monthly income") )
 
 
- 
+Expences = float(income) - float(expenses) 
 
+print("your expences are", Expences ("percent of your monthly income. ") )
 
 print("Thank you for using budget calculator!")
