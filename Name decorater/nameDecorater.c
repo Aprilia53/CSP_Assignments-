1fb35fb8b6d5de48fc62ee3c62129595ae066150
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-09-18 20:35:35 +0000

    ran code

--- Name decorater/nameDecorater.c
+++ Name decorater/nameDecorater.c
@@ -1,12 +1,9 @@
 #include <stdio.h> 
 int main(){
     char name[] = "your name decorated is ";
-    char prefix[] = " <<< ";
-    char prefix2[] = " Aprilia";
-    char prefix3[] = " >>>";
+    char prefix[] = " <<<Aprilia>>>";
     strcat(name, prefix);
-    strcat(prefix2, prefix3); 
-    printf("%s\n", name);
+    printf("%s\n", name); 
 
     return 0; 
 }
