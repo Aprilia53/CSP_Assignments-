2432c0044e84f9d15e8dc82f29ec36f37a9d2794
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-09-19 19:43:28 +0000

    fixed stuff

--- SillySentences.c
+++ SillySentences.c
@@ -1,14 +1,22 @@
 #include <stdio.h> 
+#include <string.h>
 int main(void) {
    char animal[20];
    char place[20];
    char verb[20];  
+   char sentence[400] = "the ";
    printf("Name an animal: ");
-   fgets(animal, sizeof(animal), stdin); 
+   scanf("%s", animal); 
    printf("Name a place: ");
-    fgets(place, sizeof(place), stdin); 
+    scanf(place, sizeof(place), stdin); 
    printf("Name a verb: ");
-    fgets(verb, sizeof(verb), stdin); 
-   printf("The %s walked to the %s and %s with his friend the angry turtle.\n", animal, place, verb);
+    scanf(verb, sizeof(verb), stdin); 
+    strcat(sentence, animal);
+    strcat(sentence, " went to the ");
+    strcat(sentence, place); 
+    strcat(sentence, " and ");
+    strcat(sentence, verb);
+    strcat(sentence, " with his friend the angry turtle.\n");
+   printf("%s", sentence);
     return 0;
 } 
