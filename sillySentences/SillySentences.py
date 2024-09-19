e6c3fd75a9221e528f3400b07c3aeb0890e27a02
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-09-19 20:26:09 +0000

    made print statments and variables

--- sillySentences/SillySentences.py
+++ sillySentences/SillySentences.py
@@ -1,6 +1,12 @@
-animal=input(); 
-place=input();
-verb=input(); 
-print("name an animal: ")
-print("name a place: ")
+print("name an animal: ") 
+animal=input()
+print("name a spacific place: ") 
+place=input()
 print("name a verb: ")
+verb=input() 
+print(f"The {animal} walked to {place} and {verb} with his bugger friend. ") 
+
+
+
+
+
