9c9dad38ae80efae5fd29cc37cb43afbd5f786ee
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-09-24 19:31:18 +0000

    made a function

---
+++ HelloWorldUpdate/HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+void name(char type[], int Aprilia){
+  printf("hello, %s !"); 
+}
+int main() {
+ 
+    return 0;
+}
---
+++ HelloWorldUpdate/HelloWorld.py
@@ -0,0 +1,8 @@
+def hello(name): 
+   return f"hello {name}!"
+
+print(hello("Leo")) 
+print(hello("Carrera")) 
+print(hello("Rossi")) 
+print(hello("Brooke")) 
+print(hello("Phil"))  
