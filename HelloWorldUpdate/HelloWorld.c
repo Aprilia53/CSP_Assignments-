3b526527d1dd65471f0e75454374f4c51eaa4dff
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-09-24 19:39:18 +0000

    made a function

--- HelloWorldUpdate/HelloWorld.c
+++ HelloWorldUpdate/HelloWorld.c
@@ -1,8 +1,12 @@
 #include <stdio.h> 
-void name(char type[], int Aprilia){
-  printf("hello, %s !"); 
+void name(char name[]){
+  printf("hello,%s! \n", name); 
 }
 int main() {
- 
+ name("brooke");
+ name("Phil");
+ name("Rossi");
+ name("Carrera");
+ name("Leo"); 
     return 0;
 }
