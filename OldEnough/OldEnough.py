5468984c2d67cc5dcf8e047f941a362fdfafff64
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-01 02:23:14 +0000

    created conditionals

---
+++ OldEnough/OldEnough.py
@@ -0,0 +1,17 @@
+age = 19
+voter = "yes" 
+
+if age >= 18 and voter == "yes":
+  print("You are able to vote!") 
+
+elif age >= 16:
+  print("You are old enough to drive!") 
+
+elif age >= 15:
+  print("You are old enough to get a learners permit!") 
+
+elif age >= 5:
+  print("You are old enough to go to school!") 
+
+else: 
+  print("You are too young to go to school.")
