66b0f90c3ba310093ce4cbf2c1a94a07e10e1c45
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-02 19:04:32 +0000

    created assignment

--- TimeOfDay/TimeOfDay.c
+++ TimeOfDay/TimeOfDay.c
@@ -4,5 +4,10 @@
 int main ()
 {
     time_t rawtime;
+    struct tm *timeinfo; 
+
+    time(&rawtime);
+    timeinfo = localtime (&rawtime);
+    printf("current local time and date: %s", asctime(timeinfo));
     return 0;
 }
