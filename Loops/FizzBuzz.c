ea01865527e8ceb8cdcc7499582ba1b7a35f62c2
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-10 19:32:52 +0000

    trying to make it work

--- Loops/FizzBuzz.c
+++ Loops/FizzBuzz.c
@@ -1,21 +1,34 @@
 #include <stdio.h> 
-int i;
+#include <string.h>
+int i; 
 int usr;
+char one;
+char two;
 int main() {
     printf("Give me a number");
     scanf("%d", &usr); 
+    printf("Give me a short word!\n");
+    scanf("%s", one); 
+    printf("Give me another short word!\n");
+    scanf("%s", two); 
     //loop that counts to 50
   for(i=0;i<=usr;i++){
+        
         //replace #'s divisable by 3 and 5 with "fizzbuzz"
-    if(i%3==0 && i&5==0){
-        printf("FizzBuzz\n ");
+    if(i%3==0 && i%5==0){
+        strcat(one,two);
+        printf("%s\n", one);
+
     }else if (i%3==0){ 
+        
         //replace #'s divisable by 3 with "Fizz"
-       printf("Fizz\n");
+       printf("%s\n", two);
     }else if(i%5==0){
+        
         //replace #'s divisable by 5 with "buzz"
         printf("Buzz\n");
     }else { 
+        
         //print the # 
     printf("%d\n", i); 
     } 
