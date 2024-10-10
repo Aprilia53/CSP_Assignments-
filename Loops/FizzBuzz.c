a95ab068d1da4592b4e4f6ade5a06e49bcd21a79
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-10 19:21:06 +0000

    made fizzbuzz

---
+++ Loops/FizzBuzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+int i;
+int main() {
+    //loop that counts to 50
+  for(i=0;i<=50;i++){
+        //replace #'s divisable by 3 and 5 with "fizzbuzz"
+    if(i%3==0 && i&5==0){
+        printf("FizzBuzz\n ");
+    }else if (i%3==0){ 
+        //replace #'s divisable by 3 with "Fizz"
+       printf("Fizz\n");
+    }else if(i%5==0){
+        //replace #'s divisable by 5 with "buzz"
+        printf("Buzz\n");
+    }else { 
+        //print the # 
+    printf("%d\n", i); 
+    } 
+   } return 0;
+}
