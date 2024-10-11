e4073f50196adc1aa233a35fc66100ac87814c05
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-11 19:17:42 +0000

    finish loops c

--- Loops/loops.c
+++ Loops/loops.c
@@ -1,8 +1,9 @@
 #include <stdio.h> 
-char sibs[8][20] = {"carrera","Rossi","Mom","dad"}
+char sibs[8][20] = {"carrera","Rossi","Mom","dad"};
+int i; 
 int main() {
-   for(i=0;i<=10,i++){
-     printf(i)
+   for(i=0;i<=3;i++){
+     printf("%s\n", sibs[i]);
    }
     return 0;
 }  
