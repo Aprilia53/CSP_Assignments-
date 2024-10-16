c14cb7e5856ef284c74818d553ad9bde8216a6fa
Aprilia53 <Aprilia.galata@ucas-edu.net>
2024-10-16 19:22:13 +0000

    made a loop

--- Loops/loops.py
+++ Loops/loops.py
@@ -1,13 +1,18 @@
-import random 
+user = int(input("give me a number\n"))
+user1 = input("give me a short word\n")
+user2 = input("give me another short word\n") 
+
+three = user1 + user2 
+
+for i in range(1, user + 1):
+    if i % 3 == 0 and i % 5 == 0:
+      print(three)
+    elif i % 3 ==0:
+        print(user1)
+    elif i % 5 ==0:
+        print(user2)
+    else: 
+        print(i) 
 
 
-count = 0 
-siblings = ["Rossi" , "Carrera",  "mom"] 
-
-
-           
-while count <  len(siblings): 
-      print(siblings[count])
-      count+=1 
-  
-      
+    
